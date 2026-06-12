cb35f51aea44bbbe73560762686063b0f9f16f37
GarRomm <romain.garcia@santor.net>
2026-06-12 22:20:56 +0200

    fix(upload): ajout de l'authentification JWT pour le téléchargement audio

--- server/src/routes/upload.js
+++ server/src/routes/upload.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const uploadMiddleware = require('../services/uploadService');
 const uploadController = require('../controllers/uploadController');
+const { authJWT } = require('../middleware/authJWT');
 
 const router = express.Router();
 
-router.post('/', uploadMiddleware.single('audio'), uploadController.uploadAudio);
+router.post('/', authJWT, uploadMiddleware.single('audio'), uploadController.uploadAudio);
 
 module.exports = router;
